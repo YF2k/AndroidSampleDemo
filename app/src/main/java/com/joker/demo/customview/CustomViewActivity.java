4edbb745c11ed9d48fee9bbc05b6cb55f3dc69d6
xiangyu.li <xiangyu.li@100credit.com>
2020-04-29 15:04:47 +0800

    自定义View

--- app/src/main/java/com/joker/demo/customview/CustomViewActivity.java
+++ app/src/main/java/com/joker/demo/customview/CustomViewActivity.java
@@ -1,17 +1,34 @@
 package com.joker.demo.customview;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.joker.demo.BaseActivity;
 import com.joker.demo.R;
 
-public class CustomViewActivity extends AppCompatActivity {
+public class CustomViewActivity extends BaseActivity implements View.OnClickListener{
+    Button mBtnPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_view);
+        initView();
 
+
+    }
+
+    private void initView() {
+        mBtnPath=findViewById(R.id.btn_path);
+        mBtnPath.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case  R.id.btn_path:
+                startTargetActivity(PathActivity.class);
+                break;
+        }
     }
 }
