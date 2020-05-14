4700663deacfec1091364eded5ecbde0e7dd0b12
xiangyu.li <xiangyu.li@100credit.com>
2020-05-14 18:06:24 +0800

    解决butterknife 崩溃问题

--- app/src/main/java/com/joker/demo/butterknife/ButterKnifeActivity.java
+++ app/src/main/java/com/joker/demo/butterknife/ButterKnifeActivity.java
@@ -1,36 +1,54 @@
 package com.joker.demo.butterknife;
 
 import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindString;
 import butterknife.BindView;
+import butterknife.BindViews;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import butterknife.Unbinder;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.joker.demo.R;
+
+import java.util.List;
 
 public class ButterKnifeActivity extends AppCompatActivity {
     private Unbinder unbinder;
 
     @BindView(R.id.tv_title)
-    TextView mTvTitle;
+    TextView mTvTitle;//@BindView fields must not be private or static.
+
+    @BindViews({R.id.btn_submit1, R.id.btn_submit2, R.id.btn_submit3})
+    List<Button> buttonList;
+
+    @BindString(R.string.app_name)
+    String appName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_butter_knife);
-        unbinder= ButterKnife.bind(this);
+        unbinder = ButterKnife.bind(this);
 
-
+        buttonList.get(0).setText("button1");
+        buttonList.get(1).setText("button2");
+        buttonList.get(2).setText("button3");
 
     }
 
 
     @OnClick(R.id.btn_submit)
-    public void submit(){
+    public void submit() {
         mTvTitle.setText("hello world");
+    }
+
+    @OnClick(R.id.btn_submit1)
+    public void submit1() {
+        mTvTitle.setText(appName);
     }
 
     @Override
@@ -38,4 +56,21 @@ public class ButterKnifeActivity extends AppCompatActivity {
         unbinder.unbind();
         super.onDestroy();
     }
+
+
+//adapter中使用butterknife
+    /*public class SwordsmanAdapter extends ArrayAdapter<Swordsman> {
+        @Override
+        public View getView(int position,View convertView,ViewGroup parent) {
+        }
+
+        class ViewHolder {
+            @BindView(R.id.tv_name)
+            TextView tv_name;
+
+            public ViewHolder(View view){
+                ButterKnife.bind(this,view);
+            }
+        }
+    }*/
 }
--- app/src/main/java/com/joker/demo/butterknife/XyKnifeActivity.java
+++ app/src/main/java/com/joker/demo/butterknife/XyKnifeActivity.java
@@ -3,15 +3,20 @@ package com.joker.demo.butterknife;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.joker.annotation.XyBindView;
+import com.joker.demo.BaseActivity;
 import com.joker.demo.R;
 
-public class XyKnifeActivity extends AppCompatActivity {
+public class XyKnifeActivity extends BaseActivity {
     @XyBindView(R.id.tv_xyknife)
     TextView mTvXyKnife;
 
+    @XyBindView(R.id.btn_butterknife)
+    Button mBtnButterKnife;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,11 @@ public class XyKnifeActivity extends AppCompatActivity {
         XyKnife.bind(this);
 
         mTvXyKnife.setText("hello World");
+        mBtnButterKnife.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startTargetActivity(ButterKnifeActivity.class);
+            }
+        });
     }
 }
