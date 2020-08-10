de778fa033bc472b2f890345284fe7f6d5f06062
xiangyu.li <xiangyu.li@100credit.com>
2020-08-10 15:30:07 +0800

    权限RequestCode

---
+++ app/src/main/java/com/joker/demo/content/RequestCodes.java
@@ -0,0 +1,6 @@
+package com.joker.demo.content;
+
+public class RequestCodes {
+    public static int S_REQUEST_CODE = 5000;
+    public static final int REQUEST_CODE_CHECK_PERMISSION = ++S_REQUEST_CODE;
+}
